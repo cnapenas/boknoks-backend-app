92989f729d77901ef59f7d2aed7c5c73fc499ce7
Charisse Napenas <cnapenas@yahoo.com>
2024-06-16 15:43:16 +0800

    error fix in building

--- db.js
+++ db.js
@@ -14,7 +14,7 @@ mongoose.connect(`${process.env.DB_CONNECTION_STRING}${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() =>  console.log('Connected to database:', mongoose.connection.db.databaseName)
+  .then(() =>  console.log('Connected to database:', mongoose.connection.db.databaseName))
   .catch(err => console.error('Error:', err));
 
   module.exports = mongoose;
